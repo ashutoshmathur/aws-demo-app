18d83b875a47501bf6aeb1db5a57e07bd2349db8
ashutoshmathur <ashutoshmathurr@gmail.com>
2024-10-27 05:12:07 -0400

    feat: added zod dependency as a utils layer

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://upc5n8t6c5.execute-api.us-east-1.amazonaws.com/prod/products",
+  product: "https://7o334gnc82.execute-api.us-east-1.amazonaws.com/prod/products",
   order: "/prod/order",
   import: "/prod/import",
   bff: "/prod/bff",
